f4814f0b7b2fac77abfa1af612cb15d075ec2fe6
Carlos Córdova <carlos_cdo@outlook.com>
2023-09-21 19:52:50 -0500

    fix validation files

--- src/pages/Public/Document.vue
+++ src/pages/Public/Document.vue
@@ -93,7 +93,7 @@ onMounted(async () => {
         </h3>
       </section>
     </main>
-    <Files :files="files" />
+    <Files v-if="files.length > 0" :files="files" />
     <Items :items="items" />
     <Comments :documentId="document.documentId" />
     <Footer />
